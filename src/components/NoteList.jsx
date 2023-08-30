cda73c5ec23b9503689e7d06276532d2c9d653fd
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2023-08-30 14:13:55 +0430

    noteList name edited

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,37 +1,36 @@
-function NoteList({notes}) {
+function NoteList({ notes }) {
   return (
     <div className="note-container">
-    <div className="note-status">
-      <p>
-        All <span></span>
-      </p>
-      <p>
-        Completed <span>2</span>
-      </p>
-      <p>
-        Open <span>3</span>
-      </p>
-    </div>
-    <div className="note-item">
-      {notes.map((note)=>{})}
-      <div className="note-item__header">
-        <div className="title">jkf</div>
-        <div className="desc">adding a new note</div>
-        <div className="trash">&#128465;</div>
-        <div className="action">
-          <input type="checkbox" />
+      <div className="note-status">
+        <p>
+          All <span></span>
+        </p>
+        <p>
+          Completed <span></span>
+        </p>
+        <p>
+          Open <span>3</span>
+        </p>
+      </div>
+      <div className="note-item">
+        <div className="note-item__header">
+          <div className="title">jkf</div>
+          <div className="desc">adding a new note</div>
+          <div className="trash">&#128465;</div>
+          <div className="action">
+            <input type="checkbox" />
+          </div>
+        </div>
+        <div className="note-item__footer">
+          {new Date().toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+          })}
         </div>
       </div>
-      <div className="note-item__footer">
-        {new Date().toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "2-digit",
-          day: "2-digit",
-        })}
-      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default NoteList
+export default NoteList;
