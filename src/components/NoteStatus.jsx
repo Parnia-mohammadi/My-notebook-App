5d8bc32b38114784655c42a969f6a9c789d21d2a
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2023-09-03 15:49:11 +0430

    git status updated,conditional rendering added

--- src/components/NoteStatus.jsx
+++ src/components/NoteStatus.jsx
@@ -1,26 +1,36 @@
-function NoteStatus({notes}) {
-    let completedNoteNumber = 0;
-    notes.map((note) => {
-      note.completed
-        ? (completedNoteNumber += 1)
-        : (completedNoteNumber += 0);
-    });
-    return (
+function NoteStatus({ notes }) {
+  //first way
+  // let completedNoteNumber = 0;
+  // notes.map((note) => {
+  //   note.completed
+  //     ? (completedNoteNumber += 1)
+  //     : (completedNoteNumber += 0);
+  // });
+  //second way
+  const allNotes = notes.length;
+  const completedNotes = notes.filter((n) => n.completed).length;
+  //conditional rendering:
+  if(!allNotes) return <h2>No Notes has been added yet.</h2> 
+  return (
     <div className="note-status">
-        <li>
-          All <span>{notes.length}</span>
-        </li>
-        <li>
-          Completed 
-          <span>
-            {completedNoteNumber}
-          </span>
-        </li>
-        <li>
-          Open<span> 3</span>
-        </li>
-      </div>
-  )
+      <li>
+        All <span>{allNotes}</span>
+      </li>
+      <li>
+        Completed 
+        <span>
+          {
+            /* {completedNoteNumber} */
+            //second way:
+            completedNotes
+          }
+        </span>
+      </li>
+      <li>
+        Open <span>{allNotes - completedNotes}</span>
+      </li>
+    </div>
+  );
 }
 
-export default NoteStatus
+export default NoteStatus;
