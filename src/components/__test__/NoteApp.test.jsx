821bfc75652408414c2ca08b3f58031fb6cfab60
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-04-02 14:19:19 +0430

    add a function for multiple tests

--- src/components/__test__/NoteApp.test.jsx
+++ src/components/__test__/NoteApp.test.jsx
@@ -3,6 +3,16 @@ import { expect, test } from "vitest";
 import NoteApp from "../NoteApp";
 import NotesProvider from "../../context/NotesContext";
 
+function AddNote(notes) {
+  const inputTitle = screen.getByPlaceholderText(/Note Title/i);
+  const inputDescription = screen.getByPlaceholderText(/Note Description/i);
+  const button = screen.getByRole("button", { name: /Add New Note/i });
+  notes.forEach((note) => {
+    fireEvent.change(inputTitle, { target: { value: note.title } });
+    fireEvent.change(inputDescription, { target: { value: note.description } });
+    fireEvent.click(button);
+  });
+}
 test("NoteApp #1:should input be empty after submit", () => {
   render(
     <NotesProvider>
@@ -10,12 +20,13 @@ test("NoteApp #1:should input be empty after submit", () => {
     </NotesProvider>
   );
   const inputTitle = screen.getByPlaceholderText("note title ...");
-  const inputDescription = screen.getByPlaceholderText("note description ...");
-  fireEvent.change(inputTitle, { target: { value: "note one title" } });
-  fireEvent.change(inputDescription, {
-    target: { value: "note one description" },
-  });
-  const button = screen.getByRole("button", { name: /Add New Note/i });
-  fireEvent.click(button);
+  // const inputDescription = screen.getByPlaceholderText("note description ...");
+  // fireEvent.change(inputTitle, { target: { value: "note one title" } });
+  // fireEvent.change(inputDescription, {
+  //   target: { value: "note one description" },
+  // });
+  // const button = screen.getByRole("button", { name: /Add New Note/i });
+  // fireEvent.click(button);
+  AddNote([{ title: "note title one", description: "note description one" }]);
   expect(inputTitle.value).toBe("");
 });
